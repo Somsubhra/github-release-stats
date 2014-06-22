eef3b1fbf084de12a52389ccd53be3f6d6f8f75e
Somsubhra Bairi <somsubhra.bairi@gmail.com>
2014-06-23 01:41:24 +0530

    Show release details.

--- js/main.js
+++ js/main.js
@@ -49,25 +49,60 @@ function getStatsPressedCB(response) {
         html = "<div class='col-md-6 col-md-offset-3 error'>" + errMessage + "</div>";
 
     } else {
+
         html += "<div class='col-md-6 col-md-offset-3'>";
         var latest = true;
+
         $.each(data, function(index, item) {
             var releaseTag = item.tag_name;
             var releaseURL = item.html_url;
+            var releaseAssets = item.assets;
+            var hasAssets = releaseAssets.length != 0;
+            var releaseAuthor = item.author;
+            var publishDate = item.published_at.split("T")[0];
+
             if(latest) {
                 html += "<div class='row release latest-release'>" +
                     "<h2><a href='" + releaseURL + "' target='_blank'>" +
+                    "<span class='glyphicon glyphicon-tag'></span>&nbsp&nbsp" +
                     "Latest Release: " + releaseTag +
-                    "</a></h2><hr class='latest-release-hr'>" +
-                    "</div>";
+                    "</a></h2><hr class='latest-release-hr'>";
                 latest = false;
             } else {
                 html += "<div class='row release'>" +
                     "<h4><a href='" + releaseURL + "' target='_blank'>" +
+                    "<span class='glyphicon glyphicon-tag'></span>&nbsp&nbsp" +
                     releaseTag +
-                    "</a></h4><hr class='release-hr'>" +
-                    "</div>";
+                    "</a></h4><hr class='release-hr'>";
             }
+
+            html += "<h5><b><span class='glyphicon glyphicon-info-sign'></span>&nbsp&nbsp" +
+                "Release details:</b></h5>";
+
+            html += "<ul>";
+
+            html += "<li><b><span class='glyphicon glyphicon-user'></span>&nbsp&nbspRelease Author: </b>" +
+                "<a href='" + releaseAuthor.html_url + "'>" + releaseAuthor.login  +"</a><br></li>";
+
+            html += "<li><b><span class='glyphicon glyphicon-calendar'></span>&nbsp&nbspPublished on: </b>" +
+                publishDate + "</li>";
+
+            html += "</ul>";
+            if(hasAssets) {
+                html += "<h5><b><span class='glyphicon glyphicon-download'></span>" +
+                    "&nbsp&nbspDownload Info: </b>" +
+                    "</h5>";
+
+                html += "<ul>";
+                $.each(releaseAssets, function(index, asset) {
+                    var assetSize = (asset.size / 1000000.0).toFixed(2);
+                    var lastUpdate = asset.updated_at.split("T")[0];
+                    html += "<li><b>" + asset.name + "</b>(" + assetSize + "MB) - Downloaded " +
+                        asset.download_count + " times.<br><i>Last updated on " + lastUpdate + "</i></li>";
+                });
+                html += "</ul>";
+            }
+            html += "</div>";
         });
         html += "</div>";
     }
