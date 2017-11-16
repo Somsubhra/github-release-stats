19f2bf207746021ee5e35d7428192122c1373354
Daniel Rudolf <github.com@daniel-rudolf.de>
2017-11-17 00:01:01 +0100

    Add repo title when search box is hidden
    As suggested by @tooomm (https://github.com/Somsubhra/github-release-stats/issues/17#issuecomment-342116283)

--- js/main.js
+++ js/main.js
@@ -183,7 +183,8 @@ $(function() {
 
     $("#get-stats-button").click(function() {
         window.location = "?username=" + $("#username").val() +
-            "&repository=" + $("#repository").val();
+            "&repository=" + $("#repository").val() +
+            ((getQueryVariable("search") == "0") ? "&search=0" : "");
     });
 
     var username = getQueryVariable("username");
@@ -192,16 +193,20 @@ $(function() {
 
     if(username != "" && repository != "") {
         $("#username").val(username);
+        $("#title .username").text(username);
         $("#repository").val(repository);
+        $("#title .repository").text(repository);
         validateInput();
         getUserRepos();
         $(".output").hide();
         $("#description").hide();
         $("#loader-gif").show();
         getStats();
-    }
-    if(showSearch == "0") {
-        $("#search").hide();
-        $("#description").hide();
+
+        if(showSearch == "0") {
+            $("#search").hide();
+            $("#description").hide();
+            $("#title").show();
+        }
     }
 });
